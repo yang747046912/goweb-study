8174fb5a371cd4c3677e5df80be354c8474bbda5
yang747046912 <2377599811@qq.com>
2017-05-03 20:11:56 +0800

    日期格式化

--- initial/sql/sql.go
+++ initial/sql/sql.go
@@ -8,6 +8,6 @@ import (
 
 func InitSql()  {
 	orm.RegisterDriver("mysql",orm.DRMySQL)
-	orm.RegisterDataBase("default", "mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8", "root", "123456", "127.0.0.1", 3306, "app_demo"))
+	orm.RegisterDataBase("default", "mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=true&loc=Local", "root", "123456", "127.0.0.1", 3306, "app_demo"))
 	orm.Debug=true
 }
